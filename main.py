3b275105588c84b415d36d8b3c451b6e9e24fb42
Michał Hewelt <mhewelt9@gmail.com>
2024-01-08 15:51:47 +0100

    add decryption

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -18,4 +18,18 @@ def encrypt(text_to_encrypt, shift_value):
     return print(f"The encoded text is '{new_text}'")
 
 
-encrypt(text, shift)
+def decrypt(text_to_decrypt, shift_value):
+    new_text = ""
+    if abs(shift_value) > 26:
+        shift_value = shift_value % 26
+    for letter in text_to_decrypt:
+        index = alphabet.index(letter)
+        new_letter = alphabet[index - shift_value]
+        new_text += new_letter
+    return print(f"The decoded text is '{new_text}'")
+
+
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
