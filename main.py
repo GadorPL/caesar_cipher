d635b275effab11095ace6530f18c8232ccc26e7
Michał Hewelt <mhewelt9@gmail.com>
2024-01-08 17:53:08 +0100

    improve user experience, possibility of adding symbols and numbers

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+from art import logo
 
 
 def caesar(text_to_alter, shift_value, cipher_direction):
@@ -13,11 +7,31 @@ def caesar(text_to_alter, shift_value, cipher_direction):
         shift_value = shift_value % 26
     if cipher_direction == "decode":
         shift_value *= -1
-    for letter in text_to_alter:
-        index = alphabet.index(letter)
-        new_letter = alphabet[index + shift_value]
-        new_text += new_letter
+    for char in text_to_alter:
+        if char in alphabet:
+            index = alphabet.index(char)
+            new_char = alphabet[index + shift_value]
+        else:
+            new_char = char
+        new_text += new_char
     return print(f"The {cipher_direction}d text is '{new_text}'.")
 
 
-caesar(text, shift, direction)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+over = False
+
+print(logo)
+
+while not over:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+
+    restart = input("Type 'n' to finish or type anything else to restart a program")
+    if restart == "n":
+        over = True
